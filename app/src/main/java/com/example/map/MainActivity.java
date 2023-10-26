0fb443e078c73a344a556ae8d1c0a3958b30d162
eddamnatikram <dmntiikram@gmail.com>
2023-10-26 01:52:09 +0100

    first commit1

--- app/src/main/java/com/example/map/MainActivity.java
+++ app/src/main/java/com/example/map/MainActivity.java
@@ -1,59 +1,78 @@
 package com.example.map;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.location.LocationListener;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+import android.telephony.TelephonyManager;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import com.android.volley.VolleyError;
 
+
+public class MainActivity extends AppCompatActivity {
     private double latitude;
     private double longitude;
     private double altitude;
     private float accuracy;
     RequestQueue requestQueue;
-    String insertUrl = "http://192.168.168.39/localisation/createPosition.php";
+    String insertUrl = "http://192.168.168.39/map/createPosition.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnNavigateToMap = findViewById(R.id.btnNavigateToMap);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("GPS");
 
-        // Ajoutez un OnClickListener pour le bouton
-        btnNavigateToMap.setOnClickListener(new View.OnClickListener() {
+        Button btnSwitchToMapsActivity = findViewById(R.id.btnNavigateToMap);
+
+// Set an OnClickListener to handle button click
+        btnSwitchToMapsActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Code pour gérer le clic sur le bouton ici
-                // Vous pouvez lancer l'activité de la carte à l'aide d'une Intent ici
+                // Start the MapsActivity when the button is clicked
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(intent);
             }
-        });        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        });
+
+
+
+
+
+
+
+
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -65,56 +84,50 @@ public class MainActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 150, new
-                LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        latitude = location.getLatitude();
-                        longitude = location.getLongitude();
-                        altitude = location.getAltitude();
-                        accuracy = location.getAccuracy();
-                        String msg = String.format(
-                                getResources().getString(R.string.new_location),
-                                latitude, longitude, altitude, accuracy
-                        );
-                        addPosition(latitude, longitude);
-                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-                    }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 150, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                altitude = location.getAltitude();
+                accuracy = location.getAccuracy();
+                @SuppressLint("StringFormatMatches") String msg = String.format(getResources().getString(R.string.new_location), latitude, longitude, altitude, accuracy);
+                addPosition(latitude, longitude);
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
 
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-                        String newStatus = "";
-                        switch (status) {
-                            case LocationProvider.OUT_OF_SERVICE:
-                                newStatus = "OUT_OF_SERVICE";
-                                break;
-                            case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                                newStatus = "TEMPORARILY_UNAVAILABLE";
-                                break;
-                            case LocationProvider.AVAILABLE:
-                                newStatus = "AVAILABLE";
-                                break;
-                        }
-                        String msg = String.format(getResources().getString(R.string.provider_new_status), provider, newStatus);
-                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                String newStatus = "";
+                switch (status) {
+                    case LocationProvider.OUT_OF_SERVICE:
+                        newStatus = "OUT_OF_SERVICE";
+                        break;
+                    case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                        newStatus = "TEMPORARILY_UNAVAILABLE";
+                        break;
+                    case LocationProvider.AVAILABLE:
+                        newStatus = "AVAILABLE";
+                        break;
+                }
+                String msg = String.format(getResources().getString(R.string.provider_new_status), provider, newStatus);
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
 
-                    }
+            @Override
+            public void onProviderEnabled(String provider) {
+                String msg = String.format(getResources().getString(R.string.provider_enabled), provider);
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
 
-                    @Override
-                    public void onProviderEnabled(String provider) {
-                        String msg = String.format(getResources().getString(R.string.provider_enabled),
-                                provider);
-                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onProviderDisabled(String provider) {
-                        String msg = String.format(getResources().getString(R.string.provider_disabled),
-                                provider);
-                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-                    }
-                });
+            @Override
+            public void onProviderDisabled(String provider) {
+                String msg = String.format(getResources().getString(R.string.provider_disabled), provider);
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
     void addPosition(final double lat, final double lon) {
         StringRequest request = new StringRequest(Request.Method.POST, insertUrl, new Response.Listener<String>() {
             @Override
@@ -127,8 +140,7 @@ public class MainActivity extends AppCompatActivity {
         }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                TelephonyManager telephonyManager =
-                        (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+                TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
                 HashMap<String, String> params = new HashMap<String, String>();
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 params.put("latitude", lat + "");
@@ -139,7 +151,4 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         requestQueue.add(request);
-    }
-
-
-}
+    }}
